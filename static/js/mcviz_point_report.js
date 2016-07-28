3eff134f89b358e014c1b37e6800d2a00c32fb98
Peter Hawthorne <phawthorne@umn.edu>
2016-07-28 10:45:28 -0500

    dummy table added

--- static/js/mcviz_point_report.js
+++ static/js/mcviz_point_report.js
@@ -2,7 +2,20 @@
 (function (mcviz) {
     'use strict';
 
-    
+    console.log('building point-report table');
+
+    var table_holder = d3.select('#point-report')
+    	.append('table')
+    		.attr('align', 'center');
+
+    var row = table_holder.append('tbody')
+    	.append('tr')
+    row.append('td')
+		.attr('class', 'label')
+		.html('testing');
+    row.append('td')
+		.html('1234');
+
     mcviz.update_point_report = function() {
     };
 }(window.mcviz = window.mcviz || {}));
